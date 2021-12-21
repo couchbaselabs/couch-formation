153f2e93d96324d8cf515a6427d6f912aa0d848d
Michael Minichino <mminichino@mac.com>
2021-12-21 10:18:26 -0600

    Initial Revision

--- bin/populate.py
+++ bin/populate.py
@@ -136,7 +136,13 @@ class processTemplate(object):
         template_dir = os.path.dirname(self.template_file)
         self.linux_type = None
         self.linux_release = None
+        self.linux_pkgmgr = None
         self.cb_version = None
+        self.aws_image_name = None
+        self.aws_image_owner = None
+        self.aws_image_user = None
+        self.global_var_json = {}
+        self.local_var_json = {}
 
         if pargs.globals:
             self.globals_file = pargs.globals
@@ -157,7 +163,7 @@ class processTemplate(object):
         try:
             with open(self.globals_file, 'r') as inputFile:
                 global_var_text = inputFile.read()
-                global_var_json = json.loads(global_var_text)
+                self.global_var_json = json.loads(global_var_text)
             inputFile.close()
         except OSError as e:
             print("Can not read global variable file: %s" % str(e))
@@ -166,7 +172,7 @@ class processTemplate(object):
         try:
             with open(self.locals_file, 'r') as inputFile:
                 local_var_text = inputFile.read()
-                local_var_json = json.loads(global_var_text)
+                self.local_var_json = json.loads(local_var_text)
             inputFile.close()
         except OSError as e:
             print("Can not read local variable file: %s" % str(e))
@@ -188,38 +194,125 @@ class processTemplate(object):
 
         for item in requested_vars:
             if item == 'CB_VERSION':
-                if 'linux' not in global_var_json:
-                    print("Linux distribution global configuration required.")
-                    sys.exit(1)
-
-                distro_list = []
-                for key in global_var_json['linux']:
-                    distro_list.append(key)
-
-                selection = self.ask('Select Linux Distribution', distro_list)
-                self.linux_type = distro_list[selection]
-
-                version_list = []
-                version_desc = []
-                for i in range(len(global_var_json['linux'][self.linux_type])):
-                    version_list.append(global_var_json['linux'][self.linux_type][i]['version'])
-                    version_desc.append(global_var_json['linux'][self.linux_type][i]['name'])
-
-                selection = self.ask('Select Version', version_list, version_desc)
-                self.linux_release = global_var_json['linux'][self.linux_type][selection]['version']
-                linux_pkgmgr = global_var_json['linux'][self.linux_type][selection]['type']
-
-                release_list = []
                 try:
-                    cbr = cbrelease(linux_pkgmgr, self.linux_release)
-                    versions_list = cbr.get_versions()
-                    release_list = self.reverse_list(versions_list)
+                    self.get_cb_version()
                 except Exception as e:
-                    print("Can not get available releases: %s" % str(e))
+                    print("Error: %s" % str(e))
                     sys.exit(1)
+            elif item == 'LINUX_TYPE':
+                if not self.linux_type:
+                    try:
+                        self.get_linux_type()
+                    except Exception as e:
+                        print("Error: %s" % str(e))
+                        sys.exit(1)
+            elif item == 'LINUX_RELEASE':
+                if not self.linux_release:
+                    try:
+                        self.get_linux_release()
+                    except Exception as e:
+                        print("Error: %s" % str(e))
+                        sys.exit(1)
+            elif item == 'AWS_IMAGE':
+                if not self.aws_image_name:
+                    try:
+                        self.get_aws_image_name()
+                    except Exception as e:
+                        print("Error: %s" % str(e))
+                        sys.exit(1)
+            elif item == 'AWS_AMI_OWNER':
+                if not self.aws_image_owner:
+                    try:
+                        self.get_aws_image_owner()
+                    except Exception as e:
+                        print("Error: %s" % str(e))
+                        sys.exit(1)
+            elif item == 'AWS_AMI_USER':
+                if not self.aws_image_user:
+                    try:
+                        self.get_aws_image_user()
+                    except Exception as e:
+                        print("Error: %s" % str(e))
+                        sys.exit(1)
 
-                selection = self.ask('Select Couchbase Version', release_list)
-                self.cb_version = release_list[selection]
+    def get_aws_image_user(self):
+        if not self.aws_image_user:
+            try:
+                self.get_aws_image_name()
+            except Exception:
+                raise
+
+    def get_aws_image_owner(self):
+        if not self.aws_image_owner:
+            try:
+                self.get_aws_image_name()
+            except Exception:
+                raise
+
+    def get_aws_image_name(self):
+        if not self.linux_type:
+            try:
+                self.get_linux_type()
+            except Exception:
+                raise
+        if not self.linux_release:
+            try:
+                self.get_linux_release()
+            except Exception:
+                raise
+        for i in range(len(self.local_var_json['linux'][self.linux_type])):
+            if self.local_var_json['linux'][self.linux_type][i]['version'] == self.linux_release:
+                self.aws_image_name = self.local_var_json['linux'][self.linux_type][i]['image']
+                self.aws_image_owner = self.local_var_json['linux'][self.linux_type][i]['owner']
+                self.aws_image_user = self.local_var_json['linux'][self.linux_type][i]['user']
+                return True
+        raise Exception("Can not locate suitable image for %s %s linux." % (self.linux_type, self.linux_release))
+
+    def get_cb_version(self):
+        if not self.linux_type:
+            try:
+                self.get_linux_type()
+            except Exception:
+                raise
+        if not self.linux_release:
+            try:
+                self.get_linux_release()
+            except Exception:
+                raise
+        try:
+            cbr = cbrelease(self.linux_pkgmgr, self.linux_release)
+            versions_list = cbr.get_versions()
+            release_list = self.reverse_list(versions_list)
+        except Exception:
+            raise
+
+        selection = self.ask('Select Couchbase Version', release_list)
+        self.cb_version = release_list[selection]
+
+    def get_linux_release(self):
+        version_list = []
+        version_desc = []
+        if 'linux' not in self.global_var_json:
+            raise Exception("Linux distribution global configuration required.")
+
+        for i in range(len(self.global_var_json['linux'][self.linux_type])):
+            version_list.append(self.global_var_json['linux'][self.linux_type][i]['version'])
+            version_desc.append(self.global_var_json['linux'][self.linux_type][i]['name'])
+
+        selection = self.ask('Select Version', version_list, version_desc)
+        self.linux_release = self.global_var_json['linux'][self.linux_type][selection]['version']
+        self.linux_pkgmgr = self.global_var_json['linux'][self.linux_type][selection]['type']
+
+    def get_linux_type(self):
+        distro_list = []
+        if 'linux' not in self.global_var_json:
+            raise Exception("Linux distribution global configuration required.")
+
+        for key in self.global_var_json['linux']:
+            distro_list.append(key)
+
+        selection = self.ask('Select Linux Distribution', distro_list)
+        self.linux_type = distro_list[selection]
 
     def reverse_list(self, list):
         return [item for item in reversed(list)]
