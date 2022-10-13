95d0a7508e84d53c380f6edf7cd075b861d99e13
Michael Minichino <mminichino@mac.com>
2022-10-13 17:48:33 -0500

    Bug fixes

--- lib/logfile.py
+++ lib/logfile.py
@@ -7,7 +7,7 @@ import os
 
 class log_file(object):
 
-    def __init__(self, name, path=None, filename=None, level=None, overwrite=True):
+    def __init__(self, name, path=None, filename=None, level=None):
         if filename:
             self.default_debug_file = path + '/' + filename if path else filename
         else:
@@ -20,23 +20,14 @@ class log_file(object):
         self.debug = False
         default_level = 3
 
-        if overwrite:
-            try:
-                open(self.debug_file, 'w').close()
-            except Exception as err:
-                print(f"warning: can not clear log file {self.debug_file}: {err}")
-
         try:
-            default_level = int(os.environ['CLOUD_MGR_DEBUG_FILE']) if 'CLOUD_MGR_DEBUG_FILE' in os.environ else 1
+            default_level = int(os.environ['CLOUD_MGR_DEBUG_LEVEL']) if 'CLOUD_MGR_DEBUG_LEVEL' in os.environ else 1
         except ValueError:
-            print(f"warning: ignoring logging: environment variable CLOUD_MGR_DEBUG_FILE should be a number")
+            print(f"warning: ignoring logging: environment variable CLOUD_MGR_DEBUG_LEVEL should be a number")
 
         self.debug_level = level if level else default_level
 
         try:
-            if overwrite:
-                open(self.debug_file, 'w').close()
-
             if self.debug_level == 0:
                 self._logger.setLevel(logging.DEBUG)
             elif self.debug_level == 1:
@@ -54,3 +45,9 @@ class log_file(object):
     @property
     def logger(self):
         return self._logger
+
+    def clear(self):
+        try:
+            open(self.debug_file, 'w').close()
+        except Exception as err:
+            print(f"warning: can not clear log file {self.debug_file}: {err}")
